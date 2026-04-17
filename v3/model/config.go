396af606dbd3f1037d3d6b7eac9ed31e3eaee47f
Bigsk <i@ianxia.com>
2026-04-17 13:51:07 +0800

    Added a convenience type C

--- v3/model/config.go
+++ v3/model/config.go
@@ -2,7 +2,7 @@ package model
 
 type Config struct {
 	// Platforms' credentials
-	Credentials map[string]map[string]string
+	Credentials C
 	// JSON
 	Unmarshal func(data []byte, v any) error
 	Marshal   func(v any) ([]byte, error)
