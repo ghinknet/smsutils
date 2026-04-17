d8cace825aaba3666a7394ce44e6e49583630be8
Bigsk <i@ianxia.com>
2026-04-17 12:56:01 +0800

    Added return value for with update method to support chained invocation

--- v3/errors/errors.go
+++ v3/errors/errors.go
@@ -13,40 +13,45 @@ func (e *SmsutilsError) Error() string {
 	return e.message
 }
 
-func (e *SmsutilsError) WithDriverName(driverName string) {
+func (e *SmsutilsError) WithDriverName(driverName string) *SmsutilsError {
 	e.driverName = driverName
+	return e
 }
 
 func (e *SmsutilsError) DriverName() string {
 	return e.driverName
 }
 
-func (e *SmsutilsError) WithDriverCode(code int) {
+func (e *SmsutilsError) WithDriverCode(code int) *SmsutilsError {
 	e.driverCode = code
+	return e
 }
 
 func (e *SmsutilsError) DriverCode() int {
 	return e.driverCode
 }
 
-func (e *SmsutilsError) WithDriverMessage(message string) {
+func (e *SmsutilsError) WithDriverMessage(message string) *SmsutilsError {
 	e.driverMessage = message
+	return e
 }
 
 func (e *SmsutilsError) DriverMessage() string {
 	return e.driverMessage
 }
 
-func (e *SmsutilsError) WithDriverRequestID(requestID string) {
+func (e *SmsutilsError) WithDriverRequestID(requestID string) *SmsutilsError {
 	e.driverRequestID = requestID
+	return e
 }
 
 func (e *SmsutilsError) DriverRequestID() string {
 	return e.driverRequestID
 }
 
-func (e *SmsutilsError) WithDriverResponse(driverResponse any) {
+func (e *SmsutilsError) WithDriverResponse(driverResponse any) *SmsutilsError {
 	e.driverResponse = driverResponse
+	return e
 }
 
 func (e *SmsutilsError) DriverResponse() any {
